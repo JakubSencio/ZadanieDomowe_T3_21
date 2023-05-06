3f68c97c9a3cbc61eb8575948fc62740419fc9c7
senci <senci@JACOB>
2023-05-06 22:29:35 +0200

    Add project files.

---
+++ Klasa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieDomowe_T3_21
+{
+    class Klasa
+    {
+        public string Imie;
+
+        public string Miejscowosc;
+
+        public DateTime DataUrodzenia;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieDomowe_T3_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Klasa klasa = new Klasa();
+            Console.WriteLine("Czesc, jak masz na imie?");
+            var Imie = klasa.Imie = Console.ReadLine();
+
+            Console.WriteLine("Gdzie mieszkasz?");
+            var Miejscowosc = klasa.Miejscowosc = Console.ReadLine();
+
+            Console.WriteLine("W ktorym roku sie urodziles?");
+
+            var dateOfBirth = int.Parse(Console.ReadLine());
+            var wynik = 2023 - dateOfBirth;
+
+            Console.WriteLine($"Masz na imie {Imie} i mieszkasz w {Miejscowosc} oraz masz {wynik} lat.");
+
+        }
+    }
+}
